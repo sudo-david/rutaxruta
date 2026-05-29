42d9269227ed9f5c9a695900b9a03de0ffa24799
sudo-david <david11duquevelez@outlook.com>
2026-05-29 13:05:32 -0500

    Se corrigio la navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar"; // Importamos tu nuevo Navbar
+
 
 export const metadata = {
   title: "RutaXRuta - Medellín",
@@ -10,7 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body>
-        <Navbar /> {/* El Navbar ahora aparecerá arriba en todas las pantallas */}
         {children}
       </body>
     </html>
--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'admin',      // Tu usuario de MySQL Workbench
+  user: 'root',      // Tu usuario de MySQL Workbench
   password: 'david1011', // Tu contraseña de MySQL
   database: 'rutaxruta',
   waitForConnections: true,
